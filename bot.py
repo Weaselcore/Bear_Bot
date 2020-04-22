d5c025ab6c1eda64b120f92f887500117a899182
Weaselcore <weaselcorp@hotmail.com>
2020-04-22 15:39:58 +1200

    Added logging

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import json
 import discord
 import time
 import random
+import logging
 
 from discord.ext import commands
 
@@ -15,6 +16,13 @@ bot = commands.Bot(command_prefix='#', help_command=None)
 
 with open('token.json', 'r') as file_to_read:
     token = json.load(file_to_read)
+
+# Sick of lack of warnings, this initialises logging to file.
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 
 # First thing the bot runs.
