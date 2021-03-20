7c2963bb8787816119bb76bdfa579156299122b5
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2021-03-20 13:31:19 +1300

    Rename folders for consistency

--- cogs/logger.py
+++ cogs/logger.py
@@ -7,7 +7,7 @@ import motor.motor_asyncio
 # needed to parse passwords with @ symbols.
 import urllib
 from discord.ext import commands, tasks
-from BBLibrary.ConfigUtil import *
+from bblib.ConfigUtil import *
 
 # Default interval is 15 minutes.
 # Initialised outside of class as a constant.
